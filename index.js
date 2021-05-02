4e25f2e4386af55098a9c668fcb723eb80e3ec44
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-02 15:17:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(multiply)
 let random = Math.floor(Math.random() * 10) + 1;
 console.log(random)
 
-num3 = 99
-num4 = 5
-let mod = num3 % num4
-console.log(mod)
+// num3 = 99
+// num4 = 5
+// let mod = num3 % num4
+// console.log(mod)
