e9fa7d118c81761d9859ab47ae90ca809bbf183d
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-02 15:13:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ let num2 = 2
 let multiply = num1 * num2
 console.log(multiply)
 
-let random = Math.floor(Math.random() * 10) + 1
+let random = Math.floor(Math.random() * 10) + 1;
 console.log(random)
